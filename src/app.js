a38e396fbb92ea6a28de15b3820082fa9e190740
wvdelden99 <66563216+wvdelden99@users.noreply.github.com>
2023-04-24 15:50:52 +0200

    sidenav update

--- src/app.js
+++ src/app.js
@@ -1,25 +1,19 @@
 // Nav Hamburger
 const menuBtn = document.querySelector(".nav-hamburger-button");
 const sideNav = document.querySelector(".sidenav");
-const menuSided = document.querySelector(".menu-sided_wrapper");
-const menuOverlay = document.querySelector(".menu-sided_overlay")
-const borderLeft = document.querySelector(".content-body-border-left");
+const sideNavContent = document.querySelector(".sidenav-content-container");
 
 let menuOpen = false;
 menuBtn.addEventListener('click', () => {
     if (!menuOpen) {
         menuBtn.classList.add('open');
-        sideNav.classList.add('menu-sided-active');
-        menuSided.classList.add('active');
-        menuOverlay.classList.remove('d-none');
-        // borderLeft.classList.add('active_b');
+        sideNav.classList.add('sidenav-active');
+        sideNavContent.classList.add('active');
         menuOpen = true;
     } else {
         menuBtn.classList.remove('open');
-        sideNav.classList.remove('menu-sided-active');
-        menuSided.classList.remove('active');
-        menuOverlay.classList.add('d-none');
-        // borderLeft.classList.remove('active_b');
+        sideNav.classList.remove('sidenav-active');
+        sideNavContent.classList.remove('active');
         menuOpen = false;
     }
 });
